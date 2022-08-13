1525231b143c03fe0db4371bfe061e6742b7a0ef
Micodlr <dlreyesmico@gmail.com>
2022-08-12 17:59:04 -0700

    Add Users table migration and seeds

---
+++ backend/db/seeders/20220813004528-demo-user.js
@@ -0,0 +1,46 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          firstName: "Jane",
+          lastName: "Smith",
+          email: "jane.smith@gmail.com",
+          username: "JaneSmith",
+          password: bcrypt.hashSync("password"),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(
+      "Users",
+      {
+        username: { [Op.in]: ["JaneSmith"] },
+      },
+      {}
+    );
+  },
+};
