a172fd84ce0f624356a9b25f09628c5470058151
Micodlr <dlreyesmico@gmail.com>
2022-08-12 20:39:46 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,6 +6,7 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
+// Log in
 router.post("/", async (req, res, next) => {
   const { credential, password } = req.body;
 
@@ -24,6 +25,12 @@ router.post("/", async (req, res, next) => {
   return res.json({
     user,
   });
+});
+
+// Log out
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
 });
 
 module.exports = router;
