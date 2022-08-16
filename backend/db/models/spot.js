48571997c1ea2066d0f32a373a4f5dfd0a179921
Micodlr <dlreyesmico@gmail.com>
2022-08-15 21:22:04 -0700

    fixed models typo for spots

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
         onDelete: "CASCADE",
       });
       Spot.hasMany(models.Review, { foreignKey: "spotId" });
-      Spot.hasMany(modesl.Image, {
+      Spot.hasMany(models.Image, {
         foreignKey: "imageableId",
         constraints: false,
         scope: {
